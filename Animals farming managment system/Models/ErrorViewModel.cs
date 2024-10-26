81c35df4d2a0573ef46377091989626f6e0ffab1
KafiaBahkam <KafiaBahkam@DESKTOP-UK1C1SN>
2024-10-26 11:25:46 +0300

    Add project files.

---
+++ Animals farming managment system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Animals_farming_managment_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
